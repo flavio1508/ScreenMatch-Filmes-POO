838f99f3b511c75c7fd6ce0a96c28383b06b62e4
flavio1508 <flavioaugusto1508@gmail.com>
2023-06-27 11:36:19 -0300

    Criando metodos

--- src/Filmes.java
+++ src/Filmes.java
@@ -1,8 +1,21 @@
-public class Filmes{
+public class Filmes {
     String nome;
     int anoDeLancamento;
     Boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasAvaliacao;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibiFichaTecnica() {
+        System.out.println("nome do filme" + nome);// acesso o atributo
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacao += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacao / totalDeAvaliacoes;
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,17 @@
 public class Principal {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Filmes meuFilme = new Filmes();
         meuFilme.nome = "Homem de ferro";
         meuFilme.anoDeLancamento = 2008;
         meuFilme.duracaoEmMinutos = 180;
 
-        System.out.println(meuFilme.nome);//acesso o atributo
+        meuFilme.exibiFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.somaDasAvaliacao);
+        System.out.println(meuFilme.totalDeAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
     }
 }
