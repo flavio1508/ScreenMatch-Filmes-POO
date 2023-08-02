1a2be868a0b6aa91e8a8163994059e087b7a3d19
Flávio Augusto <flavioaugusto1508@gmail.com>
2023-08-02 14:33:35 -0400

    Mesmo nome, comportamentos diferentes

--- src/Principal.java
+++ src/Principal.java
@@ -4,17 +4,17 @@ import br.com.alura.screenmatch.modelo.Serie;
 public class Principal {
     public static void main(String[] args) {
         Filmes meuFilme = new Filmes();
-        meuFilme.setNome( "Homem de ferro");
-        meuFilme.setAnoDeLancamento(  2008);
-        meuFilme.setDuracaoEmMinutos( 180);
+        meuFilme.setNome("Homem de ferro");
+        meuFilme.setAnoDeLancamento(2008);
+        meuFilme.setDuracaoEmMinutos(180);
+        System.out.println("Duração do filme:" + meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
 
-        System.out.println("Total de avaliações:" + meuFilme.getTotalDeAvaliacoes()
-);
+        System.out.println("Total de avaliações:" + meuFilme.getTotalDeAvaliacoes());
 
         System.out.println(meuFilme.pegaMedia());
         // meuFilme.somaDasAvaliacao = 10;
@@ -23,5 +23,10 @@ public class Principal {
 
         Serie maquinaMortifera = new Serie();
         maquinaMortifera.setNome("Maquina Mortifera");
+        maquinaMortifera.setAnoDeLancamento(2000);
+        maquinaMortifera.setEpisodiosPorTemporada(10);
+        maquinaMortifera.setMinutosPorEpisodio(50);
+        System.out.println("Duração do filme: " + maquinaMortifera.getDuracaoEmMinutos());
+        
     }
 }
